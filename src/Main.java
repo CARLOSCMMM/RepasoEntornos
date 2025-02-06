092646551756a8389e30a574622c2947ee138926
CARLOSCMMM <ccarmol2104@g.educaand.es>
2025-02-06 19:34:41 +0100

    realización de metodo numaleatorio

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,27 @@
-
 import java.util.Random;
-
 
 public class Main {
     public static void main(String[] args) {
         
         System.out.printf("Hello and welcome!\n");
        
-        Random random = new Random();
-        int numero = random.nextInt(10 + 1);  
+        int numero = generarNumeroAleatorio(1, 10);  // Genera un número aleatorio entre 1 y 10
         
         System.out.println("Número generado: " + numero);
         verificarParImpar(numero);
     }
+
     public static void verificarParImpar(int numero) {
         if (numero % 2 == 0) {
             System.out.println(numero + " es un número par.");
         } else {
             System.out.println(numero + " es un número impar.");
         }
+    }
+
+    // Método para generar un número aleatorio dentro de un rango específico
+    public static int generarNumeroAleatorio(int min, int max) {
+        Random random = new Random();
+        return random.nextInt((max - min) + 1) + min;
     }
 }
