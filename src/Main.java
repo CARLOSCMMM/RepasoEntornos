229bdf26e1708d01c9824cf12613c077977d216e
CARLOSCMMM <ccarmol2104@g.educaand.es>
2025-02-06 19:19:54 +0100

    corregido el metodo integrado en el main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
 
+import java.util.Random;
+
+
 public class Main {
     public static void main(String[] args) {
         
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        System.out.printf("Hello and welcome!\n");
+       
+        Random random = new Random();
+        int numero = random.nextInt(10 + 1);  
+        
+        System.out.println("Número generado: " + numero);
+        verificarParImpar(numero);
+    }
+    public static void verificarParImpar(int numero) {
+        if (numero % 2 == 0) {
+            System.out.println(numero + " es un número par.");
+        } else {
+            System.out.println(numero + " es un número impar.");
         }
     }
 }
--- src/MetodoParImpar.java
+++
@@ -1,9 +0,0 @@
-public class MetodoParImpar {
-    public static void verificarParImpar(int numero) {
-        if (numero % 2 == 0) {
-            System.out.println(numero + " es un número par.");
-        } else {
-            System.out.println(numero + " es un número impar.");
-        }
-    }
-}
