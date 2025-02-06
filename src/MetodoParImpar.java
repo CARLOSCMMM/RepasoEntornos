185472e4750924ea97d9daaf4d73d7387d120c59
CARLOSCMMM <ccarmol2104@g.educaand.es>
2025-02-06 16:55:06 +0100

    Metodo realizado

---
+++ src/MetodoParImpar.java
@@ -0,0 +1,9 @@
+public class MetodoParImpar {
+    public static void verificarParImpar(int numero) {
+        if (numero % 2 == 0) {
+            System.out.println(numero + " es un número par.");
+        } else {
+            System.out.println(numero + " es un número impar.");
+        }
+    }
+}
